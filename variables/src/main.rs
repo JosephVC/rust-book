48903fd81337dd5e66d786281b85f8832c6eb2c6
JosephVC <josephvcardenas@gmail.com>
2019-10-01 18:44:51 -0700

    last bit of work on variables

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -58,4 +58,24 @@ fn main() {
     let five_hundred = x.0;
     let six_six_six = x.1;
     let one = x.2;
+
+    let months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+    // here we specify an array will contain five 32 bit integers
+    let a: [i32; 5] = [1,2,3,4,5];
+
+    // again, the below won't print
+    // println!("{}", a);
+
+    //this will contain five elements, all with the value 3
+    let a = [3;5];
+
+    // standard issue creating array, then creating new variables based on indexes in that array
+    let a = [1,2,3,4,5];
+
+    let first = a[0];
+    let second = a[1];
+
+    println!("{}", first);
 }
