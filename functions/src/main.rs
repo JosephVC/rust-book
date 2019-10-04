1d6a5039af2612cd673bf6250e79169754325cec
JosephVC <josephvcardenas@gmail.com>
2019-10-04 16:42:45 -0700

    more functions work

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,16 +1,46 @@
 fn main() {
 
-    println!("hello from the main function");
-    another_function();
+//     println!("hello from the main function");
+//     another_function();
 
-    argument_function(5, 6);
+//     argument_function(5, 6);
+// }
+
+// fn another_function() {
+//     println!("another function");
+// }
+
+// fn argument_function(x: i32, y: i32) {
+//     println!("{}", x);
+//     println!("{}", y);
+
+let foo = plus_one(5);
+
+println!("The vlaue of foo is {}", foo);
+
+ let y = {
+     let x = 3;
+     x + 1
+ };
+
+ println!("teh value of y is {}", y);
+    
+let x = five();
+
+println!("the value of x is {}", x);
 }
 
-fn another_function() {
-    println!("another function");
+fn five() -> i32 {
+    5
 }
 
-fn argument_function(x: i32, y: i32) {
-    println!("{}", x);
-    println!("{}", y);
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
+
+// no
+// this won't run as it does not return a value
+// in other languages, the very assignment returns the value of the assignment
+
+// let x = (let y = 6);
+// println!("{}", x);
