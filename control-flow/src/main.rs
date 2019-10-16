b96151718d78f44e9d61f84c9223bfaa50bdc454
JosephVC <josephvcardenas@gmail.com>
2019-10-15 21:55:50 -0700

    control flow through while loops

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,13 +1,46 @@
 fn main() {
 
-    let condition  = true;
-    let number = if condition {
-        5
-    } else {
-        6
+    // straight up loops
+    let mut counter = 0;
+
+    let result = loop {
+        counter +=1; 
+
+        if counter == 10 {
+            break counter * 2;
+        }
     };
 
-    println!("the value of theh number is {}", number);
+    println!("the result is {}", result);
+
+    // while loops
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{}", number);
+        number -= 1;
+    }
+
+    println!("asfafaff")
+
+    // for loops
+
+    // the below will really just loop forever
+    // loop {
+    //     println!("I will run forever!");
+    //     break;
+    // }
+
+    // let condition  = true;
+    // let number = if condition {
+    //     5
+    // } else {
+    //     6
+    // };
+
+    // println!("the value of theh number is {}", number);
+
+
     // let number = 6;
     // if number %4 == 0 {
     //     println!("number divisible by 4");
