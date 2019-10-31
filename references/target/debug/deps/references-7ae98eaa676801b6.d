de28465350bc7057f1361926f6dc95f08c0fb4e8
JosephVC <josephvcardenas@gmail.com>
2019-10-31 15:00:07 -0700

    new section on references

---
+++ references/target/debug/deps/references-7ae98eaa676801b6.d
@@ -0,0 +1,5 @@
+C:\Users\Joe\Documents\Projects\Rust\rust-book\references\target\debug\deps\references-7ae98eaa676801b6.exe: src\main.rs
+
+C:\Users\Joe\Documents\Projects\Rust\rust-book\references\target\debug\deps\references-7ae98eaa676801b6.d: src\main.rs
+
+src\main.rs:
---
+++ references/target/debug/references.d
@@ -0,0 +1,1 @@
+C:\Users\Joe\Documents\Projects\Rust\rust-book\references\target\debug\references.pdb: C:\Users\Joe\Documents\Projects\Rust\rust-book\references\src\main.rs
